5e664d397cb2105a76e0e9b95bc135e6bdd62c9d
VidalTorres <vidaltorres2001@outlook.com>
2022-06-28 10:35:37 -0600

    Primer commit

---
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private FloatingActionButton fbtnAgregar;
+
+    private Button btnCerrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCerrar = (Button) findViewById(R.id.btnCerrar);
+        fbtnAgregar = (FloatingActionButton) findViewById(R.id.agregar);
+
+        Aplicacion app = (Aplicacion) getApplication();
+        recyclerView = (RecyclerView) findViewById(R.id.recId);
+        recyclerView.setAdapter(app.getAdaptador());
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        app.getAdaptador().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int posicion = recyclerView.getChildAdapterPosition(v);
+                String dato = app.getAlumnos().get(posicion).getNombre();
+                Toast.makeText(MainActivity.this, "Se hizo click en " +dato, Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        fbtnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Nuevo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder confirmar = new AlertDialog.Builder(MainActivity.this);
+                confirmar.setTitle("¿Cerrar Aplicación?");
+                confirmar.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                confirmar.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                confirmar.show();
+            }
+        });
+    }
+}
